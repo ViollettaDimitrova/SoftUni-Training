2a7e485004e1bb8e967b2bc1e45678071c9ef6dc
VioletaDimitrova <VioletaDimitrova@DESKTOP-VMQKPC3>
2020-12-28 21:06:24 +0200

    Added new exercise

---
+++ C#-OOP-Exercises/04. CSharp-OOP-Interfaces-and-Abstraction-Exercises/04. Telephony/Smartphone.cs
@@ -0,0 +1,60 @@
+﻿namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowsabale
+    {
+        public string Browse(string URL)
+        {
+            bool isLettersOnly = this.CheckForNumbersInURL(URL);
+
+            if (isLettersOnly)
+            {
+                return $"Browsing: {URL}!";
+            }
+            else
+            {
+                return "Invalid URL!";
+            }
+
+        }
+
+        public string Call(string number)
+        {
+            bool isNumbersOnly = this.CheckForLettersInNumber(number);
+
+            if (isNumbersOnly)
+            {
+                return $"Calling... {number}";
+            }
+            else
+            {
+                return "Invalid number!";
+            }
+        }
+
+        private bool CheckForLettersInNumber(string number)
+        {
+            foreach (var symbol in number)
+            {
+                if (!char.IsDigit(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CheckForNumbersInURL(string URL)
+        {
+            foreach (var letter in URL)
+            {
+                if (char.IsDigit(letter))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
